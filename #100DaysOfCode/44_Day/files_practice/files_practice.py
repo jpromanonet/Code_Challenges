e2ad8cd36d956fa61206ee3d3f7f2e65756f1706
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-02-08 17:35:56 -0300

    upload files

--- #100DaysOfCode/44_Day/files_practice/files_practice.py
+++ #100DaysOfCode/44_Day/files_practice/files_practice.py
@@ -1,4 +1,11 @@
+# Read files content with 'read' function
+
 file = open('C:\practice_1.txt')
 file = file.read()
 file
 
+# Read files content with 'readlines' function
+
+file = open('C:\practice_1.txt')
+file = file.readlines()
+file
