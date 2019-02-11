f85ca0cf5d5f192a5bbca3d8f0571be9013ad42d
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-02-11 13:26:57 -0300

    Exporto to .xls routine added

--- #100DaysOfCode/47_Day/Exercises_With_Shelve_And_Data/exercises_with_shelve_module.py
+++ #100DaysOfCode/47_Day/Exercises_With_Shelve_And_Data/exercises_with_shelve_module.py
@@ -6,11 +6,16 @@ import shelve
 
 shelf_File = None
 profile = None
+export = None
 
 # Program logic
 
 shelf_File = shelve.open('my_profile_data')
-profile = ('Coder','25','Argentinian')
+profile = ('Coderx','21','Argentinian')
 
-shelf_File['JuanP'] = profile
+shelf_File['JuanPa'] = profile
 shelf_File.close()
+
+export = open('datos.xls','a')
+export.write(str(profile))
+export.close()
