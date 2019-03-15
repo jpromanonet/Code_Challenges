dea4cfc8d4128cc89a37035c7cec3cbcfe889568
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-03-15 15:09:53 -0300

    Some JS exercises

---
+++ #100DaysOfCode/69_Day/exercises_FCC.js
@@ -0,0 +1,53 @@
+// FreeCodeCamp | Javascript Exercise 1 | 15/03/2019
+
+    // Example
+    var firstNameLength = 0;
+    var firstName = "Ada";
+
+    firstNameLength = firstName.length;
+
+    // Setup
+    var lastNameLength = 0;
+    var lastName = "Lovelace";
+
+    // Only change code below this line.
+
+    lastNameLength = lastName.length;
+
+// FreeCodeCamp | Javascript Exercise 2 | 15/03/2019
+
+    // Example
+    var firstLetterOfFirstName = "";
+    var firstName = "Ada";
+
+    firstLetterOfFirstName = firstName[0];
+
+    // Setup
+    var firstLetterOfLastName = "";
+    var lastName = "Lovelace";
+
+    // Only change code below this line
+    firstLetterOfLastName = lastName[0];
+
+// FreeCodeCamp | Javascript Exercise 3 | 15/03/2019
+
+    // Setup
+    var myStr = "Jello World";
+
+    // Only change code below this line
+
+    myStr = "Hello World";
+
+// FreeCodeCamp | Javascript Exercise 4 | 15/03/2019
+
+    // Example
+    var firstName = "Ada";
+    var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+    // Setup
+    var lastName = "Lovelace";
+
+    // Only change code below this line.
+    var lastLetterOfLastName = lastName[lastName.length -1];
+
+// FreeCodeCamp | Javascript Exercise 5 | 15/03/2019
