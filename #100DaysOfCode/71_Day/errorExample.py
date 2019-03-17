551fb6e68965440e1fd5a78d58e4f44d799c0ad4
Juan P. Romano <programming@jpromano.net>
2019-03-17 17:19:20 -0300

    More error handling in python

--- #100DaysOfCode/71_Day/errorExample.py
+++ #100DaysOfCode/71_Day/errorExample.py
@@ -1,7 +1,9 @@
-def spam():
-    bacon()
-def bacon():
-    raise Exception('This is the error message')
-
-spam()
+import traceback
+try:
+    raise Exception('This is a Jedi Super Master')
+except:
+    errorFile = open('errorInfo.txt', 'w')
+    errorFile.write(traceback.format_exc())
+    errorFile.close()
+    print('The traceback info was written to errorInfo.txt')
 
