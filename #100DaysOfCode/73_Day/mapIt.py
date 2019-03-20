e2079d85f3df0fd5eb5bfc82e645e6443260eb85
Juan P. Romano <programming@jpromano.net>
2019-03-19 21:56:16 -0300

    mapIt.py exercise

---
+++ #100DaysOfCode/73_Day/mapIt.py
@@ -0,0 +1,20 @@
+#! python3
+
+# mapIt.py - Launches a map in the browser using an address from the 
+# command line or clipboard
+
+# Importing libraries and frameworks
+
+import webbrowser
+import sys
+
+# Defining global variables
+
+address = None
+
+# Program Logic
+
+if len(sys.argv) > 1:
+    ## Get address from command line
+    address = ' '.join(sys.argv[1:])
+
