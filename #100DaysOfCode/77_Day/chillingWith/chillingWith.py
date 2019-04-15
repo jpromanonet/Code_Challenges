8aa96293350a1a13969272a93e1bcfd09f0c5e64
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-04-15 16:07:47 -0300

    Second iteration of chillingWith

--- #100DaysOfCode/77_Day/chillingWith/chillingWith.py
+++ #100DaysOfCode/77_Day/chillingWith/chillingWith.py
@@ -2,5 +2,64 @@
 
 # chillingWith.py it's an app to list the series and movies pending to watch where you could rate them after.
 
+# --------------------------------------------------------------------------------------------------------------
+
+# Calling libraries and frameworks
+
+import random
+import os
+import datetime
+import shelve
+import sys
+
 # Declaring global variables
 
+    ## Input user parameters, DB(shelf file) and front-end doc construction environment variables.
+
+    ## Movies parameters
+movieTitle = ''
+releaseYear = ''
+orderInSaga = ''
+pendingMovie = ''
+
+    ## Series parameters
+serieTitle = ''
+numberOfSeasons = ''
+startedYear = ''
+numberOfChapters = ''
+pendingSerie = ''
+
+    ## DB Files
+moviesFile = ''
+seriesFile = ''
+
+    ## FrontEnd Files
+
+finalHtmlFile = ''
+
+# Declaring functions
+
+def addMovie(title, releaseDate, sagaNumber, pendingToWatch):
+    moviesFile = shelve.open('moviesDB')
+    movieTitle = title
+    moviesFile[movieTitle] = [title, releaseDate, sagaNumber, pendingToWatch]
+    moviesFile.close()
+
+def addSeries(title, seasons, releaseDate, chapters, pendingToWatch):
+    seriesFile = shelve.open('seriesDB')
+    serieTitle = title
+    seriesFile[serieTitle] = [title, seasons, releaseDate, chapters, pendingToWatch]
+    seriesFile.close()
+
+# Program logic
+
+print('Insert title: ')
+movieTitle = input()
+print("Insert Release Date: ")
+release = input()
+print('''Insert order in the movie saga (if it's a solo movie, leave it blank)''')
+orderInSaga = input()
+print('Pending to watch? y/n ')
+toWatch = input()
+
+addMovie(movieTitle, release, orderInSaga, toWatch)
