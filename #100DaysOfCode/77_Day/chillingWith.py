a28c0bfdb66185f7fdf662d18c4be40c5645ab39
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-04-15 16:32:07 -0300

    More functions added.

---
+++ #100DaysOfCode/77_Day/chillingWith.py
@@ -0,0 +1,93 @@
+#! python3
+
+# chillingWith.py it's an app to list the series and movies pending to watch where you could rate them after.
+
+# --------------------------------------------------------------------------------------------------------------
+
+# Calling libraries and frameworks
+
+import random
+import os
+import datetime
+import shelve
+import sys
+
+# Declaring global variables
+
+    ## Input user parameters
+optionMenu = ''
+
+    ## Movies parameters
+movieTitle = ''
+releaseYear = ''
+orderInSaga = ''
+pendingMovie = ''
+
+    ## Series parameters
+serieTitle = ''
+numberOfSeasons = ''
+startedYear = ''
+numberOfChapters = ''
+pendingSerie = ''
+
+    ## DB Files
+moviesFile = ''
+seriesFile = ''
+
+    ## FrontEnd Files
+
+finalHtmlFile = ''
+
+# Declaring functions
+
+    ## Movies Functions
+
+def addMovie(title, releaseDate, sagaNumber, pendingToWatch):
+    moviesFile = shelve.open('moviesDB')
+    movieTitle = title
+    moviesFile[movieTitle] = [title, releaseDate, sagaNumber, pendingToWatch]
+    moviesFile.close()
+
+def deleteMovie(title):
+    movieTitle = title
+    moviesFile = shelve.open('moviesDB')
+    del moviesFile[movieTitle]
+    moviesFile.close()
+
+    ## Series Functions
+
+def addSeries(title, seasons, releaseDate, chapters, pendingToWatch):
+    seriesFile = shelve.open('seriesDB')
+    serieTitle = title
+    seriesFile[serieTitle] = [title, seasons, releaseDate, chapters, pendingToWatch]
+    seriesFile.close()
+
+def deleteSeries(title):
+    serieTitle = title
+    seriesFile = shelve.open('seriesDB')
+    del seriesFile[serieTitle]
+    seriesFile.close()
+
+# User parameters
+
+print('Select an option: ')
+print('')
+print('---------------------')
+print('Add a movie........ 1')
+print('')
+print('Add a serie........ 2')
+print('')
+print('---------------------')
+print('Delete a movie..... 3')
+print('')
+print('Delete a serie..... 4')
+print('')
+print('---------------------')
+print('Open List.......... 5')
+print('---------------------')
+print('Close.............. 6')
+optionMenu = input()
+
+# Program logic
+#if (optionMenu == '1'):
+    #addMovie()
