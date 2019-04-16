9ba48f56e0dfa3e42c8a2dd127a399a76c7dc5a7
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-04-16 17:07:20 -0300

    Last update for the 79th day.

---
+++ #100DaysOfCode/79_Day/openSocial.py
@@ -0,0 +1,13 @@
+#! python3
+
+# socialOpen.py opens the sites i frequently often (this is an exercise)
+
+# Import frameworks and libraries
+
+import webbrowser
+import sys
+
+# Program Logic
+
+webbrowser.open('https://www.reddit.com/')
+webbrowser.open('https://medium.com/')
